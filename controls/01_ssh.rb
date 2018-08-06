6e843f4ae5b59dcdfd6a388dd3ebaaaaee187f80
Jerry Aldrich <jerryaldrichiii@gmail.com>
2018-08-06 13:04:23 -0700

    Fix `01-ssh.rb` control
    Signed-off-by: Jerry Aldrich <jerryaldrichiii@gmail.com>

--- controls/01_ssh.rb
+++ controls/01_ssh.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 control '1-disable-ssh' do
-  cmd = 'Get-VMhost | Get-VMHostService | Where {$_.key -eq "TSM-SSH" -and $_.running -eq $False}'
   title 'Disable SSH'
   impact 0.5
-  desc '
-  Disable Secure Shell (SSH) for each ESXi host to prevent remote access to the ESXi shell.
-  only enable if needed for troubleshooting or diagnostics.
-  '
+  desc <<-DESCRIPTION
+    Disable Secure Shell (SSH) for each ESXi host to prevent remote access to
+    the ESXi shell. Only enable if needed for troubleshooting or diagnostics.
+  DESCRIPTION
 
-  describe powercli_command(cmd, conn_options) do
+  cmd = 'Get-VMhost | Get-VMHostService | Where {$_.key -eq "TSM-SSH" -and $_.running -eq $False}'
+  describe powercli_command(cmd) do
     its('exit_status') { should cmp 0 }
     its('stdout') { should_not cmp '' }
   end
